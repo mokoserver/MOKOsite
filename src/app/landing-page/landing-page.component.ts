0a952babb26776caf9dbe39584552370248a2d41
Levap4797 <pavelpavlov4797@gmail.com>
2019-02-16 20:54:36 +0300

    1)Update NavBar

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -9,7 +9,16 @@ export class LandingPageComponent implements OnInit {
 
   constructor() { }
 
+  isMenuOpen = true;
+  isSubMemuOpen = true;
   ngOnInit() {
   }
 
+  changeMenuState() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  changeSubMenuState(){
+    this.isSubMemuOpen=!this.isSubMemuOpen;
+  }
 }
